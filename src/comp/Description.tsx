01f05007014c5ce8d70f43f9ba0fb4858e115c9e
Matthew Briggs <matthew.briggs04@gmail.com>
2025-02-05 23:25:19 -0500

    documentation / description and styles development

--- src/comp/Description.tsx
+++ src/comp/Description.tsx
@@ -8,7 +8,9 @@ function Description({structure}: DescriptionProps) {
         <div className="description-wrapper">
             {structure == "linkedlist" &&
                 <div className="description-container linkedlist-desc-container">
-
+                    <h1>What is a Linked List?</h1>
+                    <p>Linked list are a fundamental data structure that store information in nodes, in which each node is <em>linked</em> to eachother through a pointer to the next (sometimes also the previous) node. <blockquote>Currently, the linked list shown in the visualizer is a singly linked list, which means each element is only linked to the next element. A doubly linked list would </blockquote>
+                    </p>
                 </div>
             }
             {structure == "bst" &&
@@ -27,12 +29,14 @@ function Description({structure}: DescriptionProps) {
                             <li>If the target value is larger than the current node, move to the right subtree</li>
                             <li>Repeat until either the value is found, or a NULL node is reached (value not found)</li>
                         </ol>
-                        Searching can be implemented recursively or by using a stack (see Stack). {/*might want to link to the stack data structure*/}
+                        Searching can be implemented recursively or iteratively by using a stack.
                     </p>
                     <h3>Insertion</h3>
+                    <p>Insertion follows similarly to searching.</p>
                     <h3>Deletion</h3>
+                    <p>Deletion gets a little bit more complex. What happens if we want to delete a node that has two children?</p>
                     <h3>Disadvantages</h3>
-                    <p>Consider inserting the following values in the order which they are listed (try it yourself!): [10, 8, 5, 3, 2, 1]. <blockquote><b>Pro tip:</b> into any tree visualizer, elements can be quickly inserted by putting them in an array format. [10, 8, 5] will insert a 10, then 8, then 5.</blockquote>Notice the structure of the BST. In this case, the BST is effectively a Linked List -- the time complexity for inserting and searching is O(n). Thus, the worst-case time complexity for a BST is O(n).</p>
+                    <p>Consider inserting the following values in the order which they are listed (try it yourself!): [10, 8, 5, 3, 2, 1]. <blockquote><b>Pro tip:</b> into any tree visualizer, elements can be quickly inserted by putting them in an array format. [10, 8, 5] will insert a 10, then 8, then 5.</blockquote>Notice the structure of the BST. In this case, the BST is effectively a Linked List -- the time complexity for inserting and searching is O(n). Thus, the worst-case time complexity for a BST is O(n). With a <em>balanced</em> binary search tree (such as an AVL Tree), the searching and inserting time complexity is O(logn) -- much better!</p>
                     {/* <p>See the code below for an example of how a general purpose BST would be implemented</p>
                     Code Block Here */}
                     
