d5a87b97c1e9f10d4ce0b917338c6e0e7a1a84d4
Matthew Briggs <matthew.briggs04@gmail.com>
2025-03-25 02:28:49 -0400

    updated BST description

--- src/comp/Description.tsx
+++ src/comp/Description.tsx
@@ -18,7 +18,8 @@ function Description({structure}: DescriptionProps) {
                     <h1>What is a Linked List?</h1>
                     <p>Linked lists are a fundamental data structure that store information in nodes, in which each node is <em>linked</em> to eachother through a pointer to the next and or the previous node.</p> 
                     <blockquote>Currently, the linked list shown in the visualizer is a singly linked list, which means each element contains only a pointer to the next element. A doubly linked list would also include an arrow going backwards, storing both a pointer to the next element and previous element. Note the electrical ground symbol is also used to denote <em>NULL</em> (end of list).</blockquote>
-                    Linked lists are commonly used to create other fundamental datastructures, such as a stack or queue. The "competition" to linked lists are arrays, which is what they are often compared to when making design decisions and considering the advantages and disadvantages.
+                    <p>
+                    Linked lists are commonly used to create other fundamental datas tructures, such as a stack or queue. The "competition" to linked lists are arrays, which is what they are often compared to when making design decisions and considering the advantages and disadvantages. Much of the time, a linked list solution can also be replicated with an array, and vice versa. Understanding which one to use by weighing their pros and cons given a situation is an essential skill when working with data structures.</p>
                     <h3>Advantages</h3>
                     <ul>
                         <li>Dynamically Sized. Easy to create and add a new node or remove a node already in the list. In contrast, arrays entire structure have to be resized to add more elements.</li>
@@ -26,10 +27,11 @@ function Description({structure}: DescriptionProps) {
                     </ul>
                     <h3>Disadvantages</h3>
                     <ul>
-                        <li>No random access / indexing: while in an array to access the fifth element one might type my_arr[5], a linked list would require a traversal along five of the links.
+                        <li>No random access / indexing: while an array can access its fifth element with arr[5], a linked list would require a traversal of five links.
                         </li>
                         <li>Memory overhead: Each node requires additional space to store the next pointer (the link).</li>
                     </ul>
+                    <h3>C Code Implementation</h3>
                     <CodeBlock language="c" filepath="/code_examples/C/linkedlist.c" />
                 </div>
             }
@@ -42,25 +44,54 @@ function Description({structure}: DescriptionProps) {
 
                     A BST's ordering property makes it efficient at searching, inserting, and deleting.</p>
                     <h3>Searching</h3>
-                    <p>A BST search can be summarized in a few steps
+                    <p>Consider searching for an element with value <em>target</em>.
                         <ol>
-                            <li>Start at the root of current tree (if implemented recursively, this may be a subtree)</li>
-                            <li>If the target value is smaller than the current node, move to the left subtree</li>
-                            <li>If the target value is larger than the current node, move to the right subtree</li>
-                            <li>Repeat until either the value is found, or a NULL node is reached (value not found)</li>
+                            <li>Start at the root of current tree (the top node).</li>
+                            <li>Compare the target value to the current node value.</li>
+                            <ul>
+                                <li>If it's smaller, move left.</li>
+                                <li>If it's larger, move right.</li>
+                                <li>If it matches, the node has been found! Return.</li>
+                            </ul>
+                            <li>Repeat until either the target is found, or a NULL node is reached (the target is not in the tree).</li>
                         </ol>
-                        Searching can be implemented recursively or iteratively by using a stack.
+                        Searching can be implemented recursively -- where each step calls the function on a smaller subtree -- or iteratively using a loop and pointer. The code segment below demonstrates an iterative approach.
                     </p>
                     <h3>Insertion</h3>
-                    <p>Insertion follows similarly to searching.</p>
+                    <p>Insertion follows similarly to searching:
+                    <ol>
+                        <li>Exactly matching how searching works, compare the target value to the current node. Smaller goes left, larger goes right. As discussed in the note in the introduction, the equality case is a design choice.</li>
+                        <li>Once a NULL node is found, replace it with the new node. For example, if a tree has only a 10, then inserting a 7 would check the left node of the 10, see it is NULL, and place the 7 to the left of 10.</li>
+                    </ol>
+                    </p>
                     <h3>Deletion</h3>
-                    <p>Deletion gets a little bit more complex. What happens if we want to delete a node that has two children?</p>
+                    <p>Deletion gets a little bit more complex. First, search for the node. Once the desired node has been reached, there are three main cases to consider:
+                    <ol>
+                        <li><strong>No children</strong>: Simply remove the node.</li>
+                        <li><strong>One child</strong>: Replace the node with its child.</li>
+                        <li><strong>Two children</strong>: Unfortunately, you cannot just delete the node or randomly replace the node with one of its children. Instead:</li>
+                        <ul>
+                            <li>Find the inorder successor, which is the smallest value in the right subtree. This can be found by traversing once to the right, then left as far as possible. Then, Copy the successor value into the target node, then delete the successor. Since we have gone left as far as possible, we know the successor node has at maximum only one child (it's left child must be NULL).</li>
+                            <li>This works because the inorder successor is the next largest value, so by replacing the node with it, the left child must still be smaller and the right still larger.</li>
+                            <li>Alternatively, the inorder predecessor also works (go left, then right as far as possible).</li>
+                        </ul>
+                    </ol>
+                    It is highly encouraged to test all three of these situations using the visualizer: see if you can predict what the tree will look like after deletion!
+                    </p>
                     <h3>Disadvantages</h3>
-                    <p>Consider inserting the following values in the order which they are listed (try it yourself!): [10, 8, 5, 3, 2, 1]. </p>
+                    <p>Consider inserting the following values in the order which they are listed: [10, 8, 5, 3, 2, 1]</p>
+                    <p>Since all of the elements are less than the previously inserted one, they form a straight line. In this case, the BST is effectively a Linked List -- the time complexity for inserting and searching is O(n), since every node needed to be checked. This is the Binary Search Tree's worst-case scenario, happening with data that is nearly sorted. <strong>Balanced</strong> BSTs (such as an AVL Tree) fix this issue by rearranging the nodes, enforcing an O(log n) performance. Much better!</p>
+                    
+                    <h3>Try It Yourself</h3>
+                    <p>Below are some trees you may want to insert and see how they differ in structure!</p>
+                    <ul>
+                        <li>[10, 8, 5, 3, 2, 1] - The worst case scenario from above</li>
+                        <li>[5, 3, 8, 6, 1, 10, 2] - A more balanced shape</li>
+                        <li>[10, 8, 12, 7, 9, 11] - A complete binary tree: a tree in which all but the last level is filled</li>
+                    </ul>
                     <blockquote>
-                        <b>Pro tip:</b> into any tree visualizer, elements can be quickly inserted by putting them in a list format. [10, 8, 5] will insert a 10, then 8, then 5.
+                        <b>Tip:</b> into any tree visualizer, elements can be quickly inserted by putting them in a list format. [10, 8, 5] will insert a 10, then 8, then 5.
                     </blockquote>
-                    <p>Notice the structure of the BST -- since all of the elements are less than the previously inserted one, they form a line. In this case, the BST is effectively a Linked List -- the time complexity for inserting and searching is O(n). Thus, the worst-case time complexity for a BST is O(n). With a <em>balanced</em> binary search tree (such as an AVL Tree), the searching and inserting time complexity is O(logn) -- much better!</p>
                     {/* <p>See the code below for an example of how a general purpose BST would be implemented</p>
                     Code Block Here */}
                     
