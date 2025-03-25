c4ee783daefa72f8b066bbbc695a5f2df3bc903f
Matthew Briggs <matthew.briggs04@gmail.com>
2025-03-25 01:44:10 -0400

    added linked list functions: append head, tail, free

--- public/code_examples/C/linkedlist.c
+++ public/code_examples/C/linkedlist.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 // a struct for a singular node of a linked list
 typedef struct _ListNode {
     int data; // data stored is an integer value
@@ -20,6 +19,10 @@ typedef struct _List {
 void append_tail(List* list, int value) {
     // create new node
     ListNode* new_node = malloc(sizeof(*new_node));
+    if (new_node == NULL) {
+        printf("Error: unable to malloc space for new node.\n");
+        return;
+    }
     *new_node = (ListNode) { .next = NULL, .data = value };
 
     // add new node to end of list
@@ -34,18 +37,38 @@ void append_tail(List* list, int value) {
 }
 
 void append_head(List* list, int value) {
+    ListNode* new_node = malloc(sizeof(*new_node));
+    if (new_node == NULL) {
+        printf("Error: unable to malloc space for new node.\n");
+        return;
+    }
+    // make the new node the new head
+    new_node->next = list->head;
+    new_node->data = value;
+    list->head = new_node;
 
+    // if tail is NULL (list empty) then update to be the new element
+    if (list->tail == NULL) {
+        list->tail = new_node;
+    }
+    list->size++;
 }
 
-// This function could parse an array of values -- similar to how the visualizer works
-void arr_to_list(int* arr, int size) {
-
+// this function could parse an array of values -- similar to how the visualizer works
+List arr_to_list(int* arr, int size) {
+    List list = { .head = NULL, .tail = NULL, .size = 0 };
+    for (int i = 0; i < size; i++) {
+        append_tail(&list, arr[i]); // append each element to the list
+    }
+    return list;
 }
 
 void free_list(List list) {
     ListNode* curr_node = list.head;
     while(curr_node != NULL) {
-
+        ListNode* next_node = curr_node->next;
+        free(curr_node);
+        curr_node = next_node;
         list.size--;
     }
 
